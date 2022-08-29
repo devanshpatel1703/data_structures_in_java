11d2dd0f853ef9201c8a84e2ddd7e44bc96f9389
devanshpatel1703 <86482120+devanshpatel1703@users.noreply.github.com>
2022-08-30 01:11:50 +0530

    Create add_a_new_element_in_existing_linklist.java

---
+++ add_a_new_element_in_existing_linklist.java
@@ -0,0 +1,41 @@
+public class linklist{
+    private listnode head;
+    
+    public static class listnode{
+        private int data;
+        private listnode next;
+        
+        
+        public listnode(int data){
+            this.data=data;
+            this.next=null;
+        }
+    }
+    
+    public void display(){
+        listnode current=head;
+        while(current!=null){
+            System.out.print(current.data+" --> ");
+            current=current.next;
+        }
+        System.out.print("null");
+    }
+    
+    public static void main(String [] args){
+        linklist sll=new linklist();
+        sll.head=new listnode(1);
+        listnode sec=new listnode(2);
+        listnode thr=new listnode(3);
+        listnode fif=new listnode(5);
+        sll.head.next=sec;
+        sec.next=thr;
+        thr.next=fif;
+        sll.display();
+        System.out.printf("\n");
+        listnode forth=new listnode(4);
+        thr.next=forth;
+        forth.next=fif;
+        fif.next=null;
+        sll.display();
+    }
+}
