945e6d4a5b3fc66233e21c3902036fe717ecfb91
devanshpatel1703 <86482120+devanshpatel1703@users.noreply.github.com>
2022-09-13 20:53:58 +0530

    Create Palindrome_Linked_List_leetcode.java

---
+++ Palindrome_Linked_List_leetcode.java
@@ -0,0 +1,26 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        ListNode dummy = head;
+        while(dummy!=null){
+            list.add(dummy.val);
+            dummy=dummy.next;
+        }
+        for(int i=0;i<list.size()/2;i++){
+            if(list.get(i)!=list.get(list.size()-1-i)){
+                return false;
+            }
+        }
+        return true;
+    }
+}
