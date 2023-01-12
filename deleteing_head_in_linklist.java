938d58207184ae5cdde6f31a376bbc28fe7317d9
devanshpatel1703 <86482120+devanshpatel1703@users.noreply.github.com>
2023-01-12 23:49:16 +0530

    Create deleteing_head_in_linklist.java

---
+++ deleteing_head_in_linklist.java
@@ -0,0 +1,48 @@
+class linklist{
+    static node head;
+    static class node{
+        int data;
+        node next;
+        node(int data){
+            this.data=data;
+            this.next=null;
+        }
+    }
+    public static void insert(int data){
+        node newnode=new node(data);
+        if(head==null){
+            head=newnode;   
+        }
+        else{
+            node dummy=head;
+            while(dummy.next!=null){
+                dummy=dummy.next;
+            }
+            dummy.next=newnode;
+        }
+    }
+    public static void display(){
+        node dummy=head;
+        while(dummy!=null){
+            System.out.printf("%d --> ",dummy.data);
+            dummy=dummy.next;
+        }
+        System.out.printf("null\n");
+    }
+    public static void deleteHead(){
+        head=head.next;
+    }
+    public static void main(String[] args){
+        linklist lnklst=new linklist();
+        lnklst.insert(1);
+        lnklst.insert(2);
+        lnklst.insert(3);
+        lnklst.insert(4);
+        lnklst.insert(5);
+        lnklst.insert(6);
+        lnklst.display();
+        System.out.printf("After removing the head.\n");
+        lnklst.deleteHead();
+        lnklst.display();
+    }
+}
