6fdca4e4d455bf591a9bd8912e008ca3d346db48
devanshpatel1703 <86482120+devanshpatel1703@users.noreply.github.com>
2023-01-18 00:26:57 +0530

    Create finding_starting_point_of_the_loop_in_linklist.java

---
+++ finding_starting_point_of_the_loop_in_linklist.java
@@ -0,0 +1,91 @@
+import java.util.*;
+class linklist{
+    static node head;
+    static class node{
+        int data;
+        node next;
+        node(int n){
+            this.data=n;
+            this.next=null;
+        }
+    }
+    public static void insert(int n){
+        if(head==null){
+            node newnode=new node(n);
+            head=newnode;
+        }
+        else{
+            node dummy=head;
+            while(dummy.next!=null){
+                dummy=dummy.next;
+            }
+            node newnode=new node(n);
+            dummy.next=newnode;
+        }
+    }
+    public static void print(){
+        node dummy=head;
+        while(dummy!=null){
+            System.out.printf("%d --> ",dummy.data);
+            dummy=dummy.next;
+        }
+        System.out.printf("null\n");
+    }
+    public static void loop(){
+        node dummy=head;
+        while(dummy.data!=5){
+            dummy=dummy.next;
+        }
+        node tail=head;
+        while(tail.next!=null){
+            tail=tail.next;
+        }
+        tail.next=dummy;
+    }
+    public static int loopPoint(node start){
+        node temp = head;
+        while(temp!=start){
+            temp=temp.next;
+            start=start.next;
+        }
+        return (temp.data);
+    }
+    public static boolean detectloop(){
+        node fast=head;
+        node slow=head;
+        while(fast!=null && fast.next!=null){
+            fast=fast.next.next;
+            slow=slow.next;
+            if(fast==slow){
+                int point=loopPoint(slow);
+                System.out.printf("Starting point of loop :%d\n",point);
+                return true;
+            }
+        }
+        return false;
+    }
+}
+class HelloWorld {
+    public static void main(String[] args) {
+        linklist lnk=new linklist();
+        lnk.insert(1);
+        lnk.insert(2);
+        lnk.insert(3);
+        lnk.insert(4);
+        lnk.insert(5);
+        lnk.insert(6);
+        lnk.insert(7);
+        lnk.insert(8);
+        lnk.insert(9);
+        lnk.insert(10);
+        lnk.print();
+        lnk.loop();
+        boolean check=lnk.detectloop();
+        if(check==true){
+            System.out.printf("Yes, loop is present.\n");
+        }
+        else{
+            System.out.printf("No, loop is not present.\n");
+        }
+    }
+}
