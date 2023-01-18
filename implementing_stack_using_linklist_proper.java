f9ef48e55bd1a18a72201b650af674bd312b16f2
devanshpatel1703 <86482120+devanshpatel1703@users.noreply.github.com>
2023-01-19 02:30:47 +0530

    Create implementing_stack_using_linklist_proper.java

---
+++ implementing_stack_using_linklist_proper.java
@@ -0,0 +1,68 @@
+class stack{
+    static node head;
+    static class node{
+        int data;
+        node next;
+        node(int n){
+            this.data=n;
+            this.next=null;
+        }
+    }
+    public static void putIn(int a){
+        node newnode=new node(a);
+        if(head==null){
+            head=newnode;
+        }
+        else{
+            newnode.next=head;
+            head=newnode;
+        }
+    }
+    public static void getOut(){
+        if(head==null){
+            System.out.printf("Stack is already empty.\n");
+        }
+        else{
+            int a=head.data;
+            System.out.printf("Getting out...\n");
+            System.out.printf("%d\n",a);
+            head=head.next;   
+        }
+    }
+    public static void display(){
+        if(head==null){
+            System.out.printf("Stack is already empty.\n");
+        }
+        else{
+            node dummy=head;
+            System.out.printf("CURRENT STACK : -->\n");
+            while(dummy !=null){
+                System.out.printf("    | %d |\n",dummy.data);
+                dummy=dummy.next;
+            }
+            System.out.printf("    -----\n\n");   
+        }
+    }
+}
+class Main{
+    public static void main(String[] args) {
+        stack stk=new stack();
+        stk.putIn(1);
+        stk.putIn(2);
+        stk.putIn(3);
+        stk.putIn(4);
+        stk.putIn(5);
+        stk.putIn(6);
+        stk.display();
+        stk.getOut();
+        stk.display();
+        stk.getOut();
+        stk.getOut();
+        stk.getOut();
+        stk.getOut();
+        stk.getOut();
+        stk.getOut();
+        stk.getOut();stk.getOut();stk.getOut();
+        stk.display();
+    }
+}
