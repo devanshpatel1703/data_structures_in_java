c8946c0f589e0420d975ea10e8950e2faccbe252
devanshpatel1703 <86482120+devanshpatel1703@users.noreply.github.com>
2023-03-19 23:28:14 +0530

    Create postorder_traversal_binaryree.java

---
+++ postorder_traversal_binaryree.java
@@ -0,0 +1,41 @@
+public class Main{
+    static class node{
+        int data;
+        node left;
+        node right;
+        node(int data){
+            this.data=data;
+            this.left=null;
+            this.right=null;
+        }
+    }
+    static class tree{
+        static int idx=-1;
+        public static node buildTree(int[] n){
+            idx++;
+            if(n[idx]==-1){
+                return null;
+            }
+            node newnode=new node(n[idx]);
+            newnode.left=buildTree(n);
+            newnode.right=buildTree(n);
+            return newnode;
+        }
+    }
+    public static void postorderTraversal(node root){
+        if(root==null){
+            System.out.printf("-1 ");
+            return;
+        }
+        postorderTraversal(root.left);
+        postorderTraversal(root.right);
+        System.out.printf("%d ",root.data);
+    }
+    public static void main(String[] args) {
+		int[] n={1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
+        tree t=new tree();
+        node root=t.buildTree(n);
+        System.out.printf("Root is :%d\n",root.data);
+        postorderTraversal(root);
+	}
+}
