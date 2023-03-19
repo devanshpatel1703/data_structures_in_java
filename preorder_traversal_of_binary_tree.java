1883738ee74801fb24b5e8e8ebfe062233360ca8
devanshpatel1703 <86482120+devanshpatel1703@users.noreply.github.com>
2023-03-19 23:17:30 +0530

    Create preorder_traversal_of_binary_tree.java

---
+++ preorder_traversal_of_binary_tree.java
@@ -0,0 +1,41 @@
+public class Main{
+    static class node{
+        int data;
+        node left;
+        node right;
+        node(int data){
+            this.data=data;
+            this.left=null;
+            this.right=null;
+        }
+    }
+    static class tree{
+            static int idx=-1;
+            public static node buildTree(int[] n){
+                idx++;
+                if(n[idx]==-1){
+                    return null;
+                }
+                node newnode=new node(n[idx]);
+                newnode.left=buildTree(n);
+                newnode.right=buildTree(n);
+                return newnode;
+            }
+        }
+    public static void preorderTraversal(node root){
+        if(root==null){
+            System.out.printf("-1 ");
+            return;
+        }
+        System.out.printf("%d ",root.data);
+        preorderTraversal(root.left);
+        preorderTraversal(root.right);
+    }
+    public static void main(String[] args) {
+		int[] n={1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
+        tree t=new tree();
+        node root=t.buildTree(n);
+        System.out.printf("Root is :%d\n",root.data);
+        preorderTraversal(root);
+	}
+}
